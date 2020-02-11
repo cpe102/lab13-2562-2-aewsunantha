c71ed3e075703af4a90d968dc739232101879e65
unknown <sunantha1881@gmail.com>
2020-02-11 17:45:49 +0700

    817

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -11,10 +11,32 @@ void swap(T d[],int x,int y){
 
 template <typename T>
 void insertionSort(T d[],int N){
-	
-//Write your code here.
-
+	int x=1,p; 
+	for(int t=1; t<N; t++){
+		p=x;
+		for(int j=0; j<N; j++){
+		  
+		    if(x==j) cout<<"["<<d[j]<<"]"<<" ";
+			else cout<< d[j] <<" ";
+	}
+	cout<<"=> ";
+	 for(int o=x; o>0; o--){
+		if(d[o]>d[o-1]){swap(d,o,o-1);p--;}
+		  }
+	for(int i = 0; i < 10; i++){
+			if(p==i) cout<<"["<<d[i]<<"]"<<" ";
+			else cout<< d[i] <<" ";
+    }
+	cout <<"\n";
+     	
+	for(int k=x; k>0; k--){
+		if(d[k]>d[k-1])swap(d,k ,k-1);
+			
+	}
+	x++;
 }
+}
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
@@ -26,4 +48,5 @@ int main(){
 	
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
+	return 0;
 }
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -4,9 +4,7 @@ using namespace std;
 const int N = 5;
 
 void inputMatrix(double [][N]);
-
 void findLocalMax(const double [][N], bool [][N]);
-
 void showMatrix(const bool [][N]);
 
 int main(){
@@ -14,6 +12,7 @@ int main(){
 	bool B[N][N]; 
 	cout << "Input the matrix.....\n";
 	inputMatrix(A);
+    cout<<A;
 	findLocalMax(A,B);
 	cout << "Result is \n";
 	showMatrix(B);
@@ -21,3 +20,39 @@ int main(){
 }
 
 // Write definition of inputMatrix(),matrixMultiply() and showMatrix() here
+void inputMatrix(double X[][N]){
+	 
+
+      for(int i =0; i<5 ; i++){
+		  cout<<"row "<< i+1<<": ";
+		  for(int j=0; j<5 ; j++)
+	  cin>>X[i][j];
+
+	  }
+}   
+void findLocalMax(const double S[][N], bool W[][N]){
+
+	for(int i=0; i<5; i++){
+		for(int j=0; j<5; j++){
+        if(i==0 || i==4 || j==0|| j==4 ){
+			W[i][j]=0;continue;
+		}
+	    if(S[i][j] >= S[i-1][j] && S[i][j]>= S[i+1][j] &&S[i][j] >= S[i][j-1] && S[i][j]>= S[i][j+1])
+          W[i][j]=1;
+		  else W[i][j]=0;
+		
+		  
+		}
+		
+	}
+}
+void showMatrix(const bool R[][N]){
+	for(int i=0; i<5; i++){
+		for(int j=0; j<5; j++){
+	  if(R[i][j]==1)cout<<"1 ";
+	  else cout<<"0 ";}
+	  cout<<"\n";
+	}
+	  
+
+}
